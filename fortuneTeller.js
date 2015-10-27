2342eb410331748a889baef4126a86bc08f9c634
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-10-26 17:56:45 -0700

    added more data for fortune teller

--- fortuneTeller.js
+++ fortuneTeller.js
@@ -5,10 +5,10 @@ function getRandomInt(min, max) {
 }
 
 function showMeTheFuture(){
-	var numberOfChildren =[1,2,3,4,5,6,6,6,7,9];
-	var partnerName = ["Jack","Jon", "Jane", "Sara", "Jessica", "Katie"];
-	var location = ["Salzburg, Austria", "Iraq", "SF", "china"]; 
-	var jobTitle = ["Product Manager", "Software engineer", "UX Designer", "Full Stack engineer", "Instructor"];
+	var numberOfChildren =[1,2,3,4,5,6,6,6,7,9, 1,1,1,1,,3,3,3,5,5,6,7,2,2,2,2,3,3,3];
+	var partnerName = ["Jack","Jon", "Jane", "Sara", "Jessica", "Katie", "Tall Handsome stranger", "James", "Nicole", "Lisa", "Edward", "Bella"];
+	var location = ["Salzburg, Austria", "Iraq", "SF", "china", "Oakland", "NYC", "Seattle", "Denver"]; 
+	var jobTitle = ["Product Manager", "Software engineer", "UX Designer", "Full Stack engineer", "Instructor", "Designer", "Sales Manager", "Doctor", "Data Scientist", "Data Engineer", " Quality Engineer"];
 
 	var randomJobTitle = jobTitle[getRandomInt(0, jobTitle.length)];
 	var randomLocation = location[getRandomInt(0, location.length)];
